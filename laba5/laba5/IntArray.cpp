f58f2f3482e22d3692ff0707fb94ecef2e977461
holoshumov <121400710+holoshumov@users.noreply.github.com>
2023-05-18 08:46:59 +0300

    lab5

---
+++ laba5/laba5/IntArray.cpp
@@ -0,0 +1,64 @@
+﻿#include "intArray.h"
+
+intArray::intArray()
+{
+    arr = new int[size];
+}
+
+intArray::~intArray()
+{
+    delete[] arr;
+}
+
+void intArray::IncreaseAll(int value)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] += value;
+    }
+}
+
+double intArray::GetAverage()
+{
+    if (size == 0)
+    {
+        return 0.0;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    return static_cast<double>(sum) / size;
+}
+
+void intArray::FillRandomly()
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(-100, 100);
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = distribution(generator);
+    }
+}
+
+void intArray::PrintArray()
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void intArray::DecreaseAll(double value)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] -= value;
+    }
+}
---
+++ laba5/laba5/IntArray.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "TArray.h"
+
+class intArray : public TArray
+{
+private:
+    int* arr;
+
+public:
+    intArray();
+    void IncreaseAll(int value) override;
+	void DecreaseAll(double value) override;
+	double GetAverage() override;
+    void FillRandomly() override;
+    void PrintArray() override;
+	virtual ~intArray();
+};
+
+
